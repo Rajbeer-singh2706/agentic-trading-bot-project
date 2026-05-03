a4a305d00c030977dde163a29c5d7c177eb47a72
rajbeer singh <rajbeer.singh2706@gmail.com>
2026-05-03 11:58:02 +0530

    updated

--- agentic-trading-bot_V0/data_models/models.py
+++ agentic-trading-bot_V0/data_models/models.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 from langgraph.graph.message import add_messages
 from typing import Annotated, TypedDict
+
+
 class RagToolSchema(BaseModel):
-    question:str 
+    question:str
+     
 class QuestionRequest(BaseModel):
     question: str
