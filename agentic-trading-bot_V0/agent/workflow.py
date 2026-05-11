e445231e656eb0786c0c7d9e6c575238f9f1f508
rajbeer singh <rajbeer.singh2706@gmail.com>
2026-05-11 12:07:38 +0530

    updated

--- agentic-trading-bot_V0/agent/workflow.py
+++ agentic-trading-bot_V0/agent/workflow.py
@@ -1,8 +1,10 @@
 from langgraph.graph import StateGraph, START
-from langgraph.graph.message import add_messages
+
 from langgraph.prebuilt.tool_node import ToolNode, tools_condition
 from langchain_core.messages import AIMessage, HumanMessage
-from typing_extensions import Annotated, TypedDict
+
+
+
 from utils.model_loaders import ModelLoader
 from toolkit.tools import *
 
--- src/agents/workflow.py
+++ src/agents/workflow.py
@@ -1,7 +1,26 @@
 
-from typing_extensions import Annotated
-
-
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
 
 class State(TypedDict):
-    messages: 
+    messages: Annotated[list, add_messages]
+
+class GraphBuilder:
+    def __init__(self):
+        self.model = None 
+        self.llm = None 
+        self.tools = None 
+        self.llm_with_tools = None 
+        self.graph = None  
+
+    def _chatbot_node(self,state:State):
+        return {"messages": []}
+    
+    def build(self):
+        pass 
+
+    def get_graph(self):
+        if self.graph is None:
+            raise ValueError("Graph not built. Call build() first.") 
+        return self.graph
+
